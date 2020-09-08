628bfcb878b0f1932aae96497abc77dcc2535df1
lukaszw92 <lukasz.wieczorek@op.pl>
2020-09-08 22:14:45 +0200

    models done

--- giveaway/models.py
+++ giveaway/models.py
@@ -1,4 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+
+
+class Institution(models.Model):
+    institution_types = (
+        ("1", "non-government organisation"),
+        ("2", "local collection"),
+        ("3", "foundation"),
+    )
+
+    name = models.CharField(max_length=255)
+    description = models.TextField
+    type = models.CharField(choices=institution_types, default='3', max_length=1)
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=13)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField
+    user = models.ForeignKey(User, null=True, default='Null', on_delete=models.CASCADE)
+
+
--- giveaway/views.py
+++ giveaway/views.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'registration/login.html')
+
+
+class CreateUserView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = "registration/new_user.html"
